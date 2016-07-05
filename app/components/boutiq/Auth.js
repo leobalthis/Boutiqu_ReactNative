8ebb28ca8868150cb02a6862db11e8a5fed576f7
azimitina <azimitina@yahoo.com>
2016-07-05 20:52:00 +1000

    FB login + without Navigator

--- app/components/boutiq/Auth.js
+++ app/components/boutiq/Auth.js
@@ -26,25 +26,7 @@ export class Auth extends Component {
             } else {
               AccessToken.getCurrentAccessToken().then(
                 (data) => {
-
-                  <Navigator
-                    initialRoute={{name: 'My First Scene', index: 0}}
-                    renderScene={(route, navigator) =>
-                      <Main
-                        name={route.name}
-                        onForward={() => {
-                          var nextIndex = route.index + 1;
-                          navigator.push({
-                            name: 'Scene ' + nextIndex,
-                            index: nextIndex,
-                          });
-                        }}
-                        onBack={() => {
-                          if (route.index > 0) {
-                            navigator.pop();
-                          }
-                        }}/>
-                    }/>
+                  alert('Yeeeeeeh, Welcome to Boutiq Travel :) ');
 
                   // alert(data.accessToken.toString())
                 }
