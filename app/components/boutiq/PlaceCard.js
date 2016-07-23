eb78620a56b79ff3cef2af9e7f747f88c39aecd6
azimitina <azimitina@yahoo.com>
2016-07-23 17:14:32 +1000

    [placeCard] initial part

--- app/components/boutiq/PlaceCard.js
+++ app/components/boutiq/PlaceCard.js
@@ -1,39 +1,67 @@
 import React, { Component } from 'react';
 
+import Styles from './Styles';
+
 import {
 	StyleSheet,
 	View,
 	Text,
 	Image,
+	TouchableHighlight,
 } from 'react-native';
 
 export const PlaceCard = () => {
 
 	return (
-      <Image
-        source={{uri: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/12631288_10201227918496779_8726692479317881707_n.jpg?oh=51984222db7b99543e0b811d2178df53&oe=5834F070'}}
-        style={styles.container}>
-        <Text style={styles.welcome}>
-          My favourite rooftop in NYC to enjoy a good cocktail with nice crowds and a killer view.
-        </Text>
-      </Image>
+    <TouchableHighlight>
+    	<View>
+			<Image
+			source={{uri: 'https://c1.staticflickr.com/9/8731/16524153093_2933c1ede4_b.jpg'}}
+			style={styles.container}>
+		    <Text style={styles.placeName}>
+		      Mrs Slippy
+		    </Text>		    
+		    <Text style={styles.placeP}>
+		      Double Bay, Australia
+		    </Text>
+			<Text style={styles.welcome}>
+			  My favourite rooftop in NYC to enjoy a good cocktail with nice crowds and a killer view.
+			</Text> 
+			</Image>
+		</View>
+    </TouchableHighlight>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-	flex: 1,
-		width: undefined,
-		height: 200,
+		flex: 1,
+	    width: 360,
+    	height: 150,
 		backgroundColor:'transparent',
-		justifyContent: 'center',
-		alignItems: 'center',
-		opacity: 0.5,
+		opacity: 0.7,
 	},
-	welcome: {
+	placeName: {
 		fontSize: 20,
 		color: '#fff',
-		textAlign: 'center',
-		margin: 10,
+		position: 'absolute',
+		top: 10,
+		left: 20,
+	},
+	placeP: {
+		fontSize: 14,
+		color: '#fff',
+		position: 'absolute',
+		top: 35,
+		left: 20,
+	},
+	welcome: {
+		fontSize: 15,
+		color: '#fff',
+	    width: 320,
+		top: 60,
+		left: 20,
+	    backgroundColor: 'rgba(0,0,0,0)',
+		padding: 10,
 	}
 });
