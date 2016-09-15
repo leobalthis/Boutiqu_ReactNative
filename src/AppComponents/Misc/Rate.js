5f05152b9c2a31a7eb9929781c648173e6af03f4
azimitina <azimitina@yahoo.com>
2016-09-15 22:47:06 +1000

    [rate] rateValue

--- src/AppComponents/Misc/Rate.js
+++ src/AppComponents/Misc/Rate.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
 	StyleSheet,
 	View,
 	Text,
+  TouchableOpacity,
 } from 'react-native';
+import _ from 'lodash';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Styles } from 'AppStyles';
 
@@ -15,10 +17,10 @@ const styles = StyleSheet.create({
     flexDirection:'row',
   },
   icon: {
-    fontSize: 15,
+    fontSize: 20,
   },
   rateN: {
-    marginLeft: 5,
+    marginLeft: 10,
   },
 });
 
@@ -32,13 +34,39 @@ const labelText = ({ label }) => {
   return text;
 };
 
-export const Rate = ({ label }) => (
-	<View style={styles.wrapper}>
-    {labelText({ label })}
-		<Icon name="star" style={styles.icon} />
-		<Icon name="star-o" style={styles.icon} />
-		<Icon name="star-o" style={styles.icon} />
-		<Icon name="star-o" style={styles.icon} />
-		<Icon name="star-o" style={styles.icon} />
-	</View>
-);
+export class Rate extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      rateValue: 3,
+    };
+  }
+
+  setRate(i) {
+    this.setState({ rateValue: i });
+  }
+
+  render() {
+    const { rateValue } = this.state;
+    return (
+      <View style={styles.wrapper}>
+        {_.times(rateValue, (i) => (
+          <TouchableOpacity onPress={() => {
+            this.setRate(i+1);
+          }} key={i+1}>
+    		    <Icon name="star" style={styles.icon} />
+          </TouchableOpacity>
+        ))}
+        {_.times(5-rateValue, (i) => (
+          <TouchableOpacity onPress={() => {
+            this.setRate(i+rateValue+1);
+          }} key={i+rateValue+1}>
+            <Icon name="star-o" style={styles.icon} />
+          </TouchableOpacity>
+        ))}
+    	</View>
+    );
+  }
+}  
+
